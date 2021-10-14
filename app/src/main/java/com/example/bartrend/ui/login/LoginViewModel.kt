a4dd6bcf3e7b5aab15bd2ca46439929b92b6836e
DarkSly <pedroddm96@gmail.com>
2021-10-14 16:39:47 +0100

    Added coroutine to the call in viewmodel

--- app/src/main/java/com/example/bartrend/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/bartrend/ui/login/LoginViewModel.kt
@@ -3,10 +3,12 @@ package com.example.bartrend.ui.login
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.bartrend.domain.repository.LoginRepository
 import com.example.bartrend.ui.login.model.UserLoginModel
 import com.example.bartrend.ui.login.model.UserModel
 import com.example.bartrend.ui.login.model.UserRegisterModel
+import kotlinx.coroutines.launch
 
 class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel() {
 
@@ -18,12 +20,14 @@ class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel()
 
     fun register(registerModel: UserRegisterModel): LiveData<State> {
         val result: MutableLiveData<State> = MutableLiveData(State.Loading)
-        loginRepository.register(registerModel)
-            .success {
-                result.value = State.Success(it)
-            }.failure {
-                result.value = State.Error(it)
-            }
+        viewModelScope.launch {
+            loginRepository.register(registerModel)
+                .success {
+                    result.value = State.Success(it)
+                }.failure {
+                    result.value = State.Error(it)
+                }
+        }
         return result
     }
 }
