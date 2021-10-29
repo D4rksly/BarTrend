bb4478224f7e3236ea5224b75607f799e5999543
DarkSly <pedroddm96@gmail.com>
2021-10-29 16:55:54 +0100

    Removed unnecessary classes

--- app/src/main/java/com/example/bartrend/ui/core/AppComponent.kt
+++
@@ -1,4 +0,0 @@
-package com.example.bartrend.ui.core
-
-interface AppComponent {
-}
--- app/src/main/java/com/example/bartrend/ui/core/ViewFragment.kt
+++
@@ -1,8 +0,0 @@
-package com.example.bartrend.ui.core
-
-import androidx.annotation.LayoutRes
-import androidx.fragment.app.Fragment
-
-abstract class ViewFragment(@LayoutRes private val contentLayoutId: Int): Fragment(contentLayoutId) {
-
-}
